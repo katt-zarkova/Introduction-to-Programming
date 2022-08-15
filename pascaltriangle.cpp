538ef4bd303ae2d930ac1f79a383d2a81aeb46f8
Katerina Zarkova <97607502+katt-zarkova@users.noreply.github.com>
2022-08-15 12:32:30 +0300

    Create pascaltriangle.cpp

---
+++ pascaltriangle.cpp
@@ -0,0 +1,37 @@
+//program that prints the first n rows (n<=13) of Pascal's triangle.
+#include <iostream>
+using namespace std;
+
+int binomialCoeff(int n, int k);
+
+void printPascal(int n)
+{
+    for (int line = 0; line < n; line++)
+    {
+        for (int i = 0; i <= line; i++)
+            cout << " " << binomialCoeff(line, i);
+        cout << "\n";
+    }
+}
+
+int binomialCoeff(int n, int k)
+{
+    int res = 1;
+    if (k > n - k)
+        k = n - k;
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n ;
+    printPascal(n);
+    return 0;
+}
